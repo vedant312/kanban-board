89207c45bd13fd1ec17a272f1c8bb1a9f22e23e9
vedant312 <vedantd20@iitk.ac.in>
2023-11-15 14:31:59 +0530

    Add new icons and update status and priority mappings.

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,16 +1,30 @@
-// src/utils/icons.js
-import { FaRegCircle, FaRegCheckCircle, /* other icons */ } from 'react-icons/fa';
+import {
+  FaRegCircle,
+  FaRegCheckCircle /* other icons */,
+} from 'react-icons/fa';
+import { TbAlertSquareFilled, TbDots } from 'react-icons/tb';
+import { BiSignal1, BiSignal2, BiSignal3 } from 'react-icons/bi';
+import { MdCancel } from 'react-icons/md';
 
 export const statusIcons = {
-  'Backlog': <FaRegCircle />,
-  'Todo': <FaRegCheckCircle />,
-  // ... other status mappings
+  Backlog: <FaRegCircle />,
+  Todo: <FaRegCheckCircle />,
+  'In progress': <FaRegCheckCircle />,
+  Done: <FaRegCheckCircle />,
+  Closed: <MdCancel />,
 };
 
 export const priorityIcons = {
-  0: <FaRegCircle />, // No Priority
-  1: <FaRegCheckCircle />, // Low
-  // ... other priority mappings
+  0: <TbDots />, // No Priority
+  'No Priority': <TbDots />,
+  1: <BiSignal1 />, // Low
+  Low: <BiSignal1 />,
+  2: <BiSignal2 />, // Medium
+  Medium: <BiSignal2 />,
+  3: <BiSignal3 />, // High
+  High: <BiSignal3 />,
+  4: <TbAlertSquareFilled />, // Urgent
+  Urgent: <TbAlertSquareFilled />,
 };
 
 export const userIcons = {
